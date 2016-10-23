de95c24f4dd72681ecf83429597c1735d143ceda
radist <andrey.prisniak@gmail.com>
2016-10-23 12:05:43 +0300

    Initial commit

---
+++ src/components/List/List.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+const List = props => {
+    const users = props.users;
+    return (
+        <div>
+            <ul>
+                {
+                    users.map(user => (
+                        <li key={user.id}>
+                            <a href={`/${user.id}`}>{user.name}</a>
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+};
+
+export default List;
